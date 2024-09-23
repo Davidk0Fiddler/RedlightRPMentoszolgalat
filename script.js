065bea4871b85817b5b58921707b6fc8fa32a9eb
Davidk0Fiddler <111727231+Davidk0Fiddler@users.noreply.github.com>
2024-09-23 09:30:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -77,7 +77,6 @@ function login() {
       if (usernameChecking == checker["username"] && passwordChecking == checker["password"]) {
         console.log(usernameChecking, " has logged in!")
         isLoggedIn = true
-        container.innerHTML = ` `
         landingPage()
       }
       else {
